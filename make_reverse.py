38b131796838d7c03d62724e5212064c4888f438
KOTO <olegkadushkin555@gmail.com>
2026-09-01 12:53:47 +0500

    удалил лишний код

--- make_reverse.py
+++ make_reverse.py
@@ -1,6 +1,4 @@
-import pickle
-
-def make_parents(parents):
+def make_parents(parents,db):
     if all("card" in parent for parent in parents):
         return
     for parent in parents:
@@ -9,14 +7,5 @@ def make_parents(parents):
                 db["catalogs"][id]["parents"] = db["catalogs"][parent].get("parents",[])+[parent]
             else:
                 db["cards"][id]["parents"] = db["catalogs"][parent]["parents"]+[parent]
-        make_parents(parents=db["catalogs"][parent]["children"])
+        make_parents(parents=db["catalogs"][parent]["children"],db=db)
 
-
-
-with open("dbs/makita.pkl","rb") as file:
-    db = pickle.load(file)
-    make_parents(["root"])
-    print(1,db["cards"]["card:tkanevyy-pylesbornyy-meshok-dlya-pylesosov-makitadvc260-197899-3"]["parents"])
-
-with open("makita_reverse.pkl","wb") as file2:
-    pickle.dump(db,file2)
